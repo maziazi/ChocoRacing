36176b5de87c58bd6cf27a10f9fa323d3df0d50f
maziazi <mazis9651@gmail.com>
2025-07-18 16:01:39 +0700

    Initial Commit

---
+++ ChocoRacingTests/ChocoRacingTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChocoRacingTests.swift
+//  ChocoRacingTests
+//
+//  Created by Muhamad Azis on 18/07/25.
+//
+
+import Testing
+@testable import ChocoRacing
+
+struct ChocoRacingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
