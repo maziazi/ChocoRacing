c135c20a866c159f5b01700536f4d53308e13838
Jennevelyn <jennifertjioe@outlook.com>
2025-07-25 15:24:41 +0700

    Ganti loading

--- ChocoRacing/View/SplashView.swift
+++ ChocoRacing/View/SplashView.swift
@@ -11,27 +11,34 @@ import PlayTest
 
 struct SplashView: View {
     var onLoadingComplete: () -> Void
-
+    
     @State private var isLoading = true
-
+    
     var body: some View {
         ZStack {
             Image("background")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
                 .ignoresSafeArea()
-
-            VStack(spacing: 10) {
+            
+            VStack(spacing: 0) {
                 Image("logo")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 290, height: 240)
                     .shadow(color: .black.opacity(0.3), radius: 5, x: 0, y: 3)
-
-                ProgressView("Loading Assets...")
+                    .padding(.bottom, 270)
+                
+                ProgressView()
                     .progressViewStyle(CircularProgressViewStyle(tint: .black))
-                    .foregroundColor(.black)
-                    .font(.headline)
+                    .scaleEffect(1.3)
+                    .padding(.bottom, -100)
+                
+                Image("text_loading")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 160)
+                    .padding(.top, -10)
             }
         }
         .task {
@@ -39,14 +46,14 @@ struct SplashView: View {
             onLoadingComplete()
         }
     }
-
+    
     func preloadAssets() async {
         let assetNames = [
             "CloudChunk", "Floor", "Mangkok", "PillBottle", "PLETES_Plan B_1",
             "Scene", "Skull", "SkyDome", "Slide", "SlideNCream",
             "smores", "ToyCar", "world_slide_v1"
         ]
-
+        
         for name in assetNames {
             do {
                 _ = try await Entity.load(named: name, in: playTestBundle)
